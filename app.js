30c065f626109d7cf638bbe99a75b5c079686516
afaf-tech <fatihalfikri5@gmail.com>
2020-04-29 21:17:49 +0700

    #00005

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+
+const Event = require('./models/event');
+
 
 const app = express();
-const events = [];
 
 app.use(bodyParser.json());
 
@@ -43,23 +47,42 @@ app.use(
         `),
         rootValue: {
             events: ()=> {
-                return events;
+                return Event.find().then(events=>{
+                    return events.map(event =>{
+                        // return {...event._doc, _id: event._doc._id.toString()};
+                        return {...event._doc, _id: event.id};
+                    })
+                })
             },
             createEvent: (args)=>{
-                const event = {
-                    _id : Math.random().toString(),
+              
+                const event = new Event({
                     title: args.eventInput.title,
                     description: args.eventInput.description,
                     price: +args.eventInput.price,
-                    date: new Date().toISOString()
-                }
-                console.log(event);
-                events.push(event);
-                return event;
+                    date: new Date(args.eventInput.date)
+                })
+                return event.save().then(result=>{
+                        console.log(result);
+                        return {...result._doc, _id: event.id};
+                    }).catch(err=>{
+                        console.log(err);
+                        throw err;
+                    });                
             }
         },
         graphiql:true
     })
 );
-app.listen(3000);
+
+mongoose.connect(`mongodb://localhost:27017/${process.env.MONGO_DB}`,{ useNewUrlParser: true , useUnifiedTopology:true})
+    .then(()=>{
+        console.log('db connected and server has been stated.');
+        
+        app.listen(3000);
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+
 
